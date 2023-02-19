9cfcd015cb0b56d5dd1d220220c33f4388e19542
Rufus <rsathishkumar69742@ucumberlands.edu>
2023-02-19 01:50:35 -0500

    Week 6 update

--- mrate/AppDelegate.swift
+++ mrate/AppDelegate.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MyMealRaterModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved Error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    
+    func saveContext(){
+        let context = persistentContainer.viewContext
+        do{
+            try context.save()
+        }catch{
+            let nserror = error as NSError
+            fatalError("Unresolved Error \(nserror), \(nserror.userInfo)")
+        }
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- mrate/ViewController.swift
+++ mrate/ViewController.swift
@@ -7,17 +7,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, RatingViewControllerDelegate {
+class FirstViewController: UIViewController, UITextFieldDelegate, RatingViewControllerDelegate {
 
     @IBOutlet weak var restaurantName: UITextField!
     @IBOutlet weak var mealName: UITextField!
     @IBOutlet weak var ratingValue: UILabel!
     
+    var currentRating: Restaurant?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let textFields: [UITextField] = [restaurantName, mealName]
+        
+        for textfield in textFields{
+            textfield.addTarget(self,
+                                action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)),
+                                for: UIControl.Event.editingDidEnd)
+        }
     }
 
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if currentRating == nil {
+            let context = appDelegate.persistentContainer.viewContext
+            currentRating = Restaurant(context: context)
+        }
+        currentRating?.name = restaurantName.text
+        currentRating?.meal = mealName.text
+        return true
+    }
 
     @IBAction func rateTapped(_ sender: Any) {
         self.performSegue(withIdentifier: "GetRatingView", sender: self)
@@ -32,6 +51,15 @@ class FirstViewController: UIViewController, RatingViewControllerDelegate {
     
     func setRatingInMainView(rating: String) {
         ratingValue.text = rating
+        if currentRating == nil {
+            let context = appDelegate.persistentContainer.viewContext
+            currentRating = Restaurant(context: context)
+        }
+        currentRating?.rating = Int16(rating) ?? 0
+        
+    }
+    
+    @objc func saveRating(){
+        appDelegate.saveContext()
     }
 }
-
